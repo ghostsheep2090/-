22f84cdb6151b4cf5ede8ecfce62c6eea94233bb
736773724 <736773724@qq.com>
2020-06-29 01:52:30 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,13 @@
+import allure
+
+
+class TestLogin:
+
+    def test_login_001(self):
+        assert 0
+
+    def test_login_002(self):
+        assert 1
+    @allure.step(title="测试步骤")
+    def test_login_003(self):
+        assert 1
